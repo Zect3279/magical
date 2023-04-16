618b0af05abaf507f7847b34df69883cba1a4757
Zect3279 <zeck.3279@gmail.com>
2023-04-16 17:31:34 +0900

    時間・ワード・フレーズの表示

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 # 各種リンク
 
 [プロコン dev用ページ](https://developer.textalive.jp/events/magicalmirai2023/)
+[フォント：Zen Old Mincho](https://fonts.google.com/specimen/Zen+Old+Mincho?query=zen+old)
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,38 @@ import p5 from 'p5'
 import { Player } from "textalive-app-api";
 
 const sketch = (p: p5) => {
+  let font: p5.Font
+
+  p.preload = () => {
+    font = p.loadFont("/ZenOldMincho-Medium.ttf")
+  }
+
   p.setup = () => {
-    p.createCanvas(400, 400)
+    p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL)
+    p.drawingContext.disable(p.drawingContext.DEPTH_TEST)
+    p.textFont(font)
+    p.textAlign(p.CENTER, p.CENTER)
+    p.textSize(50)
   }
 
   p.draw = () => {
-    p.background(0)
+    if (player.isPlaying) {
+      p.background(0)
+      const position = player.timer.position
+      p.text(position, 0, 70)
+      for (const phrase of player.video.phrases) {
+        for (const word of phrase.children) {
+          for (const char of word.children) {
+            if (char.startTime <= position && position < char.endTime) {
+              p.text(char.text, 0, 0)
+            }
+          }
+          if (word.startTime <= position && position < word.endTime) {
+            p.text(word.text, 0, -70)
+          }
+        }
+      }
+    }
   }
 }
 
@@ -34,4 +60,3 @@ player.addListener({
     console.log(player.data.song)
   },
 })
-
