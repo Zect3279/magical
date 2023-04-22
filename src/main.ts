1233597ab5003fde9d88640967c7c6cc278b33a5
Zect3279 <zeck.3279@gmail.com>
2023-04-22 15:32:37 +0900

    Songle APIの接続＆曲パターン表示
    フォントファイルを一緒に入れる

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import p5 from 'p5'
 import { Player } from "textalive-app-api";
 
 var endLoad = false
+const SONG_URL = "https://piapro.jp/t/ucgN/20230110005414"
+var chorus_data:any;
 
 const sketch = (p: p5) => {
   let font: p5.Font
@@ -35,7 +37,7 @@ const sketch = (p: p5) => {
       p.text(`by ${player.data.song.artist.name}`, 0, 40)
       p.textSize(20)
       p.text("画面をクリックして開始", 0, 120)
-    } else if (player.isPlaying) {
+    } else if (player.isPlaying && chorus_data) {
       p.background(0)
       p.textSize(50)
       const position = player.timer.position
@@ -49,6 +51,17 @@ const sketch = (p: p5) => {
           }
           if (word.startTime <= position && position < word.endTime) {
             p.text(word.text, 0, -70)
+          }
+        }
+      }
+      for (const s of chorus_data["repeatSegments"]) {
+        for (const r of s["repeats"]) {
+          if (r["start"] <= position && position < (r["start"] + r["duration"])) {
+            if (s["isChorus"]) {
+              p.text("サビ", 0, -160)
+            } else {
+              p.text(`${s["index"]}-${r["index"]}`, 0, -160)
+            }
           }
         }
       }
@@ -76,9 +89,15 @@ player.addListener({
   },
   onTimerReady() {
     // ↓サビ飛ばし
-    player.requestMediaSeek(15000)
+    player.requestMediaSeek(35000)
     console.log(player.data.song)
     endLoad = true
+    // https://widget.songle.jp/api/v1/song/chorus.json?url=https://piapro.jp/t/ucgN/20230110005414
+    const url = "https://widget.songle.jp/api/v1/song/chorus.json?url=" + SONG_URL
+    fetch(url)
+      .then(response => response.json())
+      .then(data => chorus_data = data)
+    // console.log(chorus_data)
     document.addEventListener('click', function(event) {
       player.requestPlay()
       console.log(player.timer.position)
